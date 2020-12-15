972d9d09ca937f4ae7552c72c200a6a6b1a8a8dc
Alan Hogarth <alan.hogarth@outlook.com>
2020-12-15 15:26:06 +0000

    MVP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [currentNumber, setCurrentNumber] = useState(0);
+    const [output, setOutput] = useState("");
+    
+    useEffect(() => {
+      if ( currentNumber % 3 === 0 & currentNumber % 5 === 0) {
+        setOutput("fizzbuz")
+      } else if (currentNumber % 3 === 0) {
+        setOutput("fizz")
+      } else if (currentNumber % 5 === 0) {
+        setOutput("buzz")
+      } else {
+        setOutput(currentNumber);
+      }
+    }, [currentNumber])
+
+
+    const handleInc = () => {
+      setCurrentNumber(currentNumber + 1);
+      
+    }
+
+
+  return(
+      <><button onClick={handleInc}>Click Me</button><br></br>
+      {currentNumber}<br></br>
+      {output}
+      </>
   );
 }
 
 export default App;
+
+
+// MVP
+// Your app should:
+
+// keep track of the "current number" in the game
+// display the correct corresponding output for the current number
+// allow the user to increment the current number.
+// You should implement React's useEffect hook in order to work out the intended output.
+
+
